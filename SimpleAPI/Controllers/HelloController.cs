06143caacbdf90d0ad0133ebf80f983466c56226
Josh <josh@harmless.ai>
2019-07-30 15:09:26 -0500

    Added Hello Controller

---
+++ SimpleAPI/Controllers/HelloController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        private static Random random = new Random();
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            var greetings = new string[] { "hello", "hi", "hey", "henlo", "hola", "oi", "sup" };
+            return greetings[random.Next(greetings.Length)];
+        }
+    }
+}
